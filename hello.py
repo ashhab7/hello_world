06127fd4ec1c52d2d835c99d29e8ef03ae55fecd
ashhab7 <m.ashhab614@gmail.com>
2024-05-10 23:18:14 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello world")
+from huggingface_hub import login
+login(token = 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)
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "meta-llama/Llama-2-7b-chat-hf",
+    cache_dir="/kaggle/working/"
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    "meta-llama/Llama-2-7b-chat-hf",
+    cache_dir="/kaggle/working/",
+    device_map="auto",
+)
